5d2751eeb003e478c9d7b8a812e5c1503ca04283
ahmad ali <ahmad.a@mu.edu.sa>
2022-08-15 13:15:28 +0300

    dashboard content

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Route::get('/test', function () {
+//    return view('admin.index');
+//});
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
